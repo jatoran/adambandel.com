a21749312d344a6e5445c56209973cba0797746d
Jatoran <adamtbandel@gmail.com>
2023-07-19 17:37:54 -0500

    df

--- tournament-idle/builder.d3c9156e.js
+++ tournament-idle/builder.d3c9156e.js
@@ -3312,7 +3312,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49473" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56034" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
